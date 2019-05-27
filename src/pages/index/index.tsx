e6ac5b34cbcf1c6739a7941ffc87ed8adc1f826d
jinhaiyue <jinhaiyue@rocoinfo.com>
2019-05-27 17:00:02 +0800

    举例

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,8 +1,46 @@
+import { ComponentClass } from 'react'
 import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View, Button } from '@tarojs/components'
+import { connect } from '@tarojs/redux'
+import { add, minus, asyncAdd } from '../../actions/counter'
 import './index.scss'
 
-export default class Index extends Component {
+type PageStateProps = {
+  counter: {
+    num: number
+  }
+}
+
+type PageDispatchProps = {
+  add: () => void
+  dec: () => void
+  asyncAdd: () => void
+}
+
+type PageOwnProps = {}
+
+type PageState = {}
+
+type IProps = PageStateProps & PageDispatchProps & PageOwnProps
+
+interface Index {
+  props: IProps
+}
+
+@connect(({ counter }) => ({
+  counter
+}), (dispatch) => ({
+  add () {
+    dispatch(add())
+  },
+  dec () {
+    dispatch(minus())
+  },
+  asyncAdd () {
+    dispatch(asyncAdd())
+  }
+}))
+class Index extends Component {
 
   /**
    * 指定config的类型声明为: Taro.Config
@@ -28,8 +66,18 @@ export default class Index extends Component {
   render () {
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
+        <Button className='add_btn' onClick={this.props.add}>+</Button>
+        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
+        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
+        <View>{this.props.counter.num}</View>
       </View>
     )
   }
 }
+
+// #region 导出注意	
+// 经过上面的声明后需要将导出的 Taro.Component 子类修改为子类本身的 props 属性	
+// 这样在使用这个子类时 Ts 才不会提示缺少 JSX 类型参数错误
+// #endregion
+
+export default Index as ComponentClass<PageOwnProps, PageState>
---
+++ src/reducers/counter.ts
@@ -0,0 +1,24 @@
+import { ADD, MINUS } from '../constants/counter'
+
+const INITIAL_STATE = {
+  num: 0
+}
+
+export default function counter (state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case ADD: 
+      return {
+        ...state,
+        num: state.num + 1
+      }
+      break
+    case MINUS: 
+      return {
+        ...state, 
+        num: state.num - 1
+      }
+      break
+    default:
+      return state
+  }
+}
